74de6400a39f38c735b0b60a02b417f9f355e05d
mysoyul <vega2k_@naver.com>
2024-03-20 17:39:04 +0900

    LectureResource using

--- src/main/java/com/boot3/myrestapi/lectures/LectureController.java
+++ src/main/java/com/boot3/myrestapi/lectures/LectureController.java
@@ -1,6 +1,8 @@
 package com.boot3.myrestapi.lectures;
 
 import com.boot3.myrestapi.lectures.dto.LectureReqDto;
+import com.boot3.myrestapi.lectures.dto.LectureResDto;
+import com.boot3.myrestapi.lectures.dto.LectureResource;
 import com.boot3.myrestapi.lectures.validator.LectureValidator;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
 @RestController
 @RequestMapping(value = "/api/lectures", produces = MediaTypes.HAL_JSON_VALUE)
@@ -30,16 +33,16 @@ public class LectureController {
     @PostMapping
     public ResponseEntity<?> createLecture(@RequestBody @Valid LectureReqDto lectureReqDto, Errors errors) {
         //입력항목 검증오류 발생했는지 체크
-        if(errors.hasErrors()) {
+        if (errors.hasErrors()) {
             return badRequest(errors);
         }
 
         //Biz로직의 입력항목 체크
         lectureValidator.validate(lectureReqDto, errors);
-        if(errors.hasErrors()) {
+        if (errors.hasErrors()) {
             return badRequest(errors);
         }
-        
+
         //ReqDto => Entity 매핑
         Lecture lecture = modelMapper.map(lectureReqDto, Lecture.class);
 
@@ -47,9 +50,20 @@ public class LectureController {
         lecture.update();
 
         Lecture addLecture = this.lectureRepository.save(lecture);
-
-        WebMvcLinkBuilder selfLinkBuilder = WebMvcLinkBuilder.linkTo(LectureController.class).slash(addLecture.getId());
+        //Entity => ResDto 매핑
+        LectureResDto lectureResDto = modelMapper.map(addLecture, LectureResDto.class);
+        WebMvcLinkBuilder selfLinkBuilder = WebMvcLinkBuilder.linkTo(LectureController.class)
+                .slash(lectureResDto.getId());
         URI createUri = selfLinkBuilder.toUri();
+
+        LectureResource lectureResource = new LectureResource(lectureResDto);
+        //relation 이름이 query-lectures 인 link
+        lectureResource.add(linkTo(LectureController.class).withRel("query-lectures"));
+        //self link
+        lectureResource.add(selfLinkBuilder.withSelfRel());
+        //relation 이름이 update-lecture 인 link
+        lectureResource.add(selfLinkBuilder.withRel("update-lecture"));
+
         return ResponseEntity.created(createUri).body(addLecture);
     }
 
