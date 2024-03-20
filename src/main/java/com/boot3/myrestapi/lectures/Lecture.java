22d3f01b13bcc242167f72bb8d324dec8624b754
mysoyul <vega2k_@naver.com>
2024-03-20 13:21:12 +0900

    entity annotation add

--- src/main/java/com/boot3/myrestapi/lectures/Lecture.java
+++ src/main/java/com/boot3/myrestapi/lectures/Lecture.java
@@ -1,6 +1,7 @@
 package com.boot3.myrestapi.lectures;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -10,8 +11,13 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @EqualsAndHashCode(of="id")
+@Entity
+@Table(name = "lectures")
 public class Lecture {
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+
+    @Column(nullable = false)
     private String name;
     private String description;
 
@@ -31,5 +37,7 @@ public class Lecture {
     private boolean offline;
 
     private boolean free;
+
+    @Enumerated(EnumType.STRING)
     private LectureStatus lectureStatus = LectureStatus.DRAFT;
 }
