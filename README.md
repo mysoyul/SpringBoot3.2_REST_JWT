d88190c382cf34c22806addb53d41471a9252f1f
mysoyul <vega2k_@naver.com>
2024-03-21 20:03:53 +0900

    swagger authorize add

---
+++ README.md
@@ -0,0 +1,8 @@
+### Lecture API
+* Swagger API docs에 Authorization 추가
+
+![Swagger Authorize](/img/swagger_auth_01.png "Swagger Authorize1")
+![Swagger Authorize](/img/swagger_auth_02.png "Swagger Authorize1")
+
+* Authorization value에 Token을 입력하고 Authorize 버튼을 클릭하면 토큰인증이 된다.
+* SpringDocsConfig 클래스에 Swagger Authorize 설정 되었습니다.
--- src/main/java/com/boot3/myrestapi/common/config/AppConfig.java
+++ src/main/java/com/boot3/myrestapi/common/config/AppConfig.java
@@ -1,8 +1,17 @@
 package com.boot3.myrestapi.common.config;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+
+import java.util.List;
 
 @Configuration
 public class AppConfig {
@@ -10,4 +19,7 @@ public class AppConfig {
     public ModelMapper modelMapper() {
         return new ModelMapper();
     }
+
+
+
 } 
