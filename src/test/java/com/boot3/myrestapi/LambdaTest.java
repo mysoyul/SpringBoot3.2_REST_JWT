44ba4a6b9fcce63c642514d55ea704bfe3baa5e5
mysoyul <vega2k_@naver.com>
2024-03-21 10:09:59 +0900

    LectureInsertRunner add

--- src/test/java/com/boot3/myrestapi/LambdaTest.java
+++ src/test/java/com/boot3/myrestapi/LambdaTest.java
@@ -9,6 +9,12 @@ import java.util.function.Consumer;
 public class LambdaTest {
     @Test
     public void runnable() {
+        /*
+           class MyRunnable implements Runnable {
+              run()
+           }
+         */
+
         //1. Anonymous Inner class
         Thread t1 = new Thread(new Runnable() {
             @Override
@@ -17,6 +23,7 @@ public class LambdaTest {
             }
         });
         t1.start();
+
         //2. Lambda Expression
         Thread t2 = new Thread(() -> System.out.println("Lambda Expression"));
         t2.start();
@@ -32,9 +39,11 @@ public class LambdaTest {
                 System.out.println("s = " + s);
             }
         });
+
         //2.Lambda Expression
         //Consumer의 추상 메서드 void accept(T t)
-        list.forEach(val -> System.out.println(val));
+        list.forEach(val -> System.out.println("값 =" + val));
+
         //3.Method Reference
         list.forEach(System.out::println);
     }
